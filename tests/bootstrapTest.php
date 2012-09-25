20a8f79d753ebf99449828c39456c50d10a72af4
Ondra <Ondra@Ondra-NB>
2012-09-26 00:02:00 +0200

    new files, tests

---
+++ tests/bootstrapTest.php
@@ -0,0 +1,20 @@
+<?php
+
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of bootstrapTest
+ *
+ * @author Ondra
+ */
+class bootstrapTest extends PHPUnit_Framework_TestCase{
+
+	
+	public function testAllwaysOk(){
+		$this->assertTrue(true);
+	}
+	
+}
