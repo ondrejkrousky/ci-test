80ce6bf7e2b549049d564079334dc0f79993dd62
Ondra <Ondra@Ondra-NB>
2012-09-26 01:14:42 +0200

    fix

--- app/bootstrap.php
+++ app/bootstrap.php
@@ -2,4 +2,25 @@
 
 echo "Hello world";
 
-
+/**
+ * Description of Hello
+ * @author Ondra Krouský <ondrej.krousky@gmail.com> 
+ */
+class Hello
+{
+	
+	/** @var array */
+	public $vars;
+	
+	/**
+	 * Hello world method.
+	 * @param array $arg
+	 * @return true 
+	 */
+	public function world(array $arg = array())
+	{
+		
+		return true;
+	}
+	
+}
